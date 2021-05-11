f3b4049a1d0469a7e0bbdc1d227068ec38ba8cdd
Filip <filipcurcic42@gmail.com>
2021-05-12 00:26:30 +0200

    add round adding

--- src/components/PlayerPoints.js
+++ src/components/PlayerPoints.js
@@ -1,6 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const PlayerPoints = ({ player, rounds, click }) => {
+  const [points, setPoints] = useState(0);
+  const test = () => {
+    console.log(points);
+  };
+  const handleChange = (e) => {
+    setPoints(e.target.value);
+  };
   console.log(rounds);
   return (
     <div className="player">
@@ -8,7 +15,9 @@ const PlayerPoints = ({ player, rounds, click }) => {
       <div className="rounds-container">
         {rounds && rounds.map((round) => <h1>{round.points}</h1>)}
       </div>
-      <button onClick={() => click(player.id)}>add round</button>
+      <button onClick={() => click(player.id, points)}>add round</button>
+      <button onClick={test}>test</button>
+      <input placeholder="Points" type="number" onChange={handleChange} />
     </div>
   );
 };
--- src/components/SessionView.js
+++ src/components/SessionView.js
@@ -11,23 +11,26 @@ import Spinner from './Spinner';
 const SessionView = ({ location }) => {
   const sessionId = location.state.id;
   const [selectedUsers, setSelectedUsers] = useState([]);
+  const [isActive, setIsActive] = useState();
 
   const { doc } = useFirestoreSingle('sessions', sessionId);
   const users = useFirestore('users');
   const sessionRef = projectFirestore.collection('sessions');
 
-  const addRound = async (playerId) => {
-    console.log(playerId);
-    // try {
-    //   await roundsRef.add({
-    //     player_id: 123,
-    //     session_id: sessionId,
-    //     points: 200,
-    //     round_number: 1,
-    //   });
-    // } catch (error) {
-    //   alert(error);
-    // }
+  const addRound = async (playerId, points) => {
+    console.log(playerId, points);
+    const newRound = {
+      player_id: playerId,
+      points,
+      round_number: 1,
+    };
+    try {
+      await sessionRef.doc(sessionId).update({
+        rounds: [...doc.rounds, newRound],
+      });
+    } catch (error) {
+      alert(error);
+    }
   };
 
   const addPlayers = async () => {
@@ -37,37 +40,9 @@ const SessionView = ({ location }) => {
       });
     } catch (error) {}
   };
-  let playerSelect;
-  // if (doc.rounds) {
-  //   playerSelect =
-  //     doc.players && doc.players.length === 0 ? (
-  //       <Select
-  //         isSearchable
-  //         placeholder="Select User"
-  //         isMulti
-  //         options={users.docs}
-  //         onChange={setSelectedUsers}
-  //       />
-  //     ) : (
-  //       <div className="players">
-  //         {doc.players &&
-  //           doc.players.map((player) => (
-  //             <PlayerPoints
-  //               player={player}
-  //               rounds={doc.rounds.filter((doc) => doc.player_id === player.id)}
-  //               click={addRound}
-  //             />
-  //           ))}
-  //       </div>
-  //     );
-  // }
-
   if (doc) {
     return (
       <div className="container">
-        <div className="session-view">
-          {doc.rounds && doc.rounds.map((round) => <h1>{round.points}</h1>)}
-        </div>
         {doc.players === undefined || doc.players.length === 0 ? (
           <h1>no players</h1>
         ) : (
@@ -101,7 +76,8 @@ const SessionView = ({ location }) => {
             </div>
           )}
         </div>
-
+        <button onClick={() => setIsActive(!isActive)}>Finish Session</button>
+        {isActive ? <h1>is active</h1> : <h2>is not active anymore</h2>}
         {/* <button onClick={test}>test</button> */}
         {/* {doc.players.length === 0 ? (
           <button onClick={addPlayers}>add players</button>
